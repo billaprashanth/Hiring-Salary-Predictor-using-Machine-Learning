6f82ae4762d445271929a88aee74a4f0a0102e78
billaprashanth <prashanthbilla590@gmail.com>
2025-05-10 19:12:30 +0530

    main changes

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-# C:\Users\prash\DataScience\DataScience\MachineLearning\Class2\Assignment2\hiring.csv
+import os
 @st.cache_data
 def load_model():
-    df = pd.read_csv("hiring.csv")
+    # Used relative path
+    file_path = os.path.join(os.path.dirname(__file__), "hiring.csv")
+    # Load and display data 
+    df = pd.read_csv(file_path)
     # Filling the nan values
     df["experience"] = df["experience"].fillna("zero")
     df["test_score(out of 10)"] = df["test_score(out of 10)"].fillna(df["test_score(out of 10)"].median())
-    # Convert word2number
+    # Convert word2number 
     from word2number import w2n
     df.experience = df.experience.apply(w2n.word_to_num)
+    st.write("Loaded Data", df.head())
     # Data Partition
     X = df[["experience","test_score(out of 10)", "interview_score(out of 10)"]]
     Y = df["salary($)"]
@@ -22,14 +26,18 @@ def load_model():
 model = load_model()
 # Input sliders
 st.sidebar.title("Hiring SDET")
-person_exp = st.sidebar.number_input("Experience", min_value=0.0)
-test_score = st.sidebar.number_input("test_score(out of 10)", min_value=0.0, max_value=10.0)
-interview_score = st.sidebar.number_input("interview_score(out of 10)", min_value=0.0, max_value=10.0)
+person_exp = st.sidebar.slider("Experience", min_value=0.0, max_value=10.0)
+test_score = st.sidebar.slider("test_score(out of 10)", min_value=0.0, max_value=10.0)
+interview_score = st.sidebar.slider("interview_score(out of 10)", min_value=0.0, max_value=10.0)
 
 st.title("Hiring Salary Predictor using Machine Learning")
-st.write("This Streamlit web app predicts the expected salary of a candidate based on their years of experience, test score, and interview performance.")
-st.write("Built using a Linear Regression model trained on real hiring data, the app provides quick and accurate salary predictions to help hiring managers or job seekers make data-driven decisions.")
-st.subheader("🧠 Powered by: Python, Pandas, Scikit-learn, and Streamlit")
+st.write("""
+This app predicts the **expected salary** of a candidate based on:
+- Years of experience  
+- Test score  
+- Interview performance  
+Built using 🧠 **Linear Regression**, **pandas**, and **Streamlit**.
+""")    
 # Predict button
 if st.button("Predict the Salary"):
     input_data = np.array([[person_exp, test_score, interview_score]])
